f2d5768dcd7b40daf185eb194cc0b103f94ab0ee
zhangyuqi <2813945288@qq.com>
2020-08-07 20:11:29 +0800

    小实训day07作业

---
+++ day07/zhangyuqi07/src/main/java/com/xiaoshu/entity/Student.java
@@ -0,0 +1,124 @@
+package com.xiaoshu.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Student implements Serializable {
+    @Id
+    private Integer sid;
+
+    private String sname;
+
+    private String gender;
+
+    private String pic;
+@DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createtime;
+
+    private Integer did;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return sid
+     */
+    public Integer getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid
+     */
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    /**
+     * @return sname
+     */
+    public String getSname() {
+        return sname;
+    }
+
+    /**
+     * @param sname
+     */
+    public void setSname(String sname) {
+        this.sname = sname == null ? null : sname.trim();
+    }
+
+    /**
+     * @return gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender
+     */
+    public void setGender(String gender) {
+        this.gender = gender == null ? null : gender.trim();
+    }
+
+    /**
+     * @return pic
+     */
+    public String getPic() {
+        return pic;
+    }
+
+    /**
+     * @param pic
+     */
+    public void setPic(String pic) {
+        this.pic = pic == null ? null : pic.trim();
+    }
+
+    /**
+     * @return createtime
+     */
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    /**
+     * @param createtime
+     */
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    /**
+     * @return did
+     */
+    public Integer getDid() {
+        return did;
+    }
+
+    /**
+     * @param did
+     */
+    public void setDid(Integer did) {
+        this.did = did;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", sid=").append(sid);
+        sb.append(", sname=").append(sname);
+        sb.append(", gender=").append(gender);
+        sb.append(", pic=").append(pic);
+        sb.append(", createtime=").append(createtime);
+        sb.append(", did=").append(did);
+        sb.append("]");
+        return sb.toString();
+    }
+}

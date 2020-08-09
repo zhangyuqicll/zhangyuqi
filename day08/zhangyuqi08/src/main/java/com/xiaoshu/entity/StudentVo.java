cdadd420b20c42811bb28ecd2e353935d0cec816
zhangyuqi <2813945288@qq.com>
2020-08-09 18:47:30 +0800

    小实训day08作业

---
+++ day08/zhangyuqi08/src/main/java/com/xiaoshu/entity/StudentVo.java
@@ -0,0 +1,15 @@
+package com.xiaoshu.entity;
+
+public class StudentVo extends Student {
+	private String cname;
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+	
+}

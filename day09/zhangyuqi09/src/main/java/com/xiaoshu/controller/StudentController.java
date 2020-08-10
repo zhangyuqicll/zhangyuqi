43ef6be9449c374083850f667272a0439f8f26ab
zhangyuqi <2813945288@qq.com>
2020-08-10 18:41:30 +0800

    小实训day09作业

---
+++ day09/zhangyuqi09/src/main/java/com/xiaoshu/controller/StudentController.java
@@ -0,0 +1,287 @@
+package com.xiaoshu.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.xiaoshu.config.util.ConfigUtil;
+import com.xiaoshu.entity.Attachment;
+import com.xiaoshu.entity.Course;
+import com.xiaoshu.entity.Log;
+import com.xiaoshu.entity.Operation;
+import com.xiaoshu.entity.Role;
+import com.xiaoshu.entity.Student;
+import com.xiaoshu.entity.StudentVo;
+import com.xiaoshu.entity.User;
+import com.xiaoshu.service.OperationService;
+import com.xiaoshu.service.RoleService;
+import com.xiaoshu.service.StudentService;
+import com.xiaoshu.service.UserService;
+import com.xiaoshu.util.StringUtil;
+import com.xiaoshu.util.TimeUtil;
+import com.xiaoshu.util.WriterUtil;
+
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
+
+@Controller
+@RequestMapping("student")
+public class StudentController extends LogController{
+	static Logger logger = Logger.getLogger(StudentController.class);
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private RoleService roleService ;
+	
+	@Autowired
+	private OperationService operationService;
+	@Autowired
+	private StudentService studentService;
+	
+	@RequestMapping("studentIndex")
+	public String index(HttpServletRequest request,Integer menuid) throws Exception{
+		List<Role> roleList = roleService.findRole(new Role());
+		List<Operation> operationList = operationService.findOperationIdsByMenuid(menuid);
+		List<Course> clist=studentService.findById();
+		request.setAttribute("clist", clist);
+		request.setAttribute("operationList", operationList);
+		request.setAttribute("roleList", roleList);
+		return "student";
+	}
+	
+	
+	@RequestMapping(value="studentList",method=RequestMethod.POST)
+	public void studentList(StudentVo studentVo, HttpServletRequest request,HttpServletResponse response,String offset,String limit) throws Exception{
+		try {
+			
+			
+			Integer pageSize = StringUtil.isEmpty(limit)?ConfigUtil.getPageSize():Integer.parseInt(limit);
+			Integer pageNum =  (Integer.parseInt(offset)/pageSize)+1;
+			PageInfo<StudentVo> userList= studentService.findUserPage(studentVo,pageNum,pageSize);
+			
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("total",userList.getTotal() );
+			jsonObj.put("rows", userList.getList());
+	        WriterUtil.write(response,jsonObj.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("用户展示错误",e);
+			throw e;
+		}
+	}
+	
+	
+	// 新增或修改
+	@RequestMapping("reserveStudent")
+	public void reserveStudent(HttpServletRequest request,Student student,HttpServletResponse response){
+		Integer id = student.getId();
+		JSONObject result=new JSONObject();
+		try {
+			if (id != null) {   // userId不为空 说明是修改
+				Student userName = studentService.existUserWithUserName(student.getName());
+				if(userName != null && userName.getId().compareTo(id)==0){
+					student.setId(id);
+					studentService.updateUser(student);
+					result.put("success", true);
+				}else{
+					result.put("success", true);
+					result.put("errorMsg", "该用户名被使用");
+				}
+				
+			}else {   // 添加
+				if(studentService.existUserWithUserName(student.getName())==null){  // 没有重复可以添加
+					studentService.addUser(student);
+					result.put("success", true);
+				} else {
+					result.put("success", true);
+					result.put("errorMsg", "该用户名被使用");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("保存用户信息错误",e);
+			result.put("success", true);
+			result.put("errorMsg", "对不起，操作失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	
+	
+	@RequestMapping("deleteStudent")
+	public void delUser(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		try {
+			String[] ids=request.getParameter("ids").split(",");
+			for (String id : ids) {
+				studentService.deleteUser(Integer.parseInt(id));
+			}
+			result.put("success", true);
+			result.put("delNums", ids.length);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("删除用户信息错误",e);
+			result.put("errorMsg", "对不起，删除失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	@RequestMapping("reserveCourse")
+	public void reserveCourse(Course course, HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		try {
+			studentService.reserveCourse(course);
+			result.put("success", true);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("删除用户信息错误",e);
+			result.put("errorMsg", "对不起，删除失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	@RequestMapping("echartsStudent")
+	public void echartsStudent(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		try {
+			List<StudentVo> elist=studentService.EchartsStudent();
+			result.put("elist", elist);
+			result.put("success", true);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("删除用户信息错误",e);
+			result.put("errorMsg", "对不起，删除失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	
+	@RequestMapping("editPassword")
+	public void editPassword(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		String oldpassword = request.getParameter("oldpassword");
+		String newpassword = request.getParameter("newpassword");
+		HttpSession session = request.getSession();
+		User currentUser = (User) session.getAttribute("currentUser");
+		if(currentUser.getPassword().equals(oldpassword)){
+			User user = new User();
+			user.setUserid(currentUser.getUserid());
+			user.setPassword(newpassword);
+			try {
+				userService.updateUser(user);
+				currentUser.setPassword(newpassword);
+				session.removeAttribute("currentUser"); 
+				session.setAttribute("currentUser", currentUser);
+				result.put("success", true);
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error("修改密码错误",e);
+				result.put("errorMsg", "对不起，修改密码失败");
+			}
+		}else{
+			logger.error(currentUser.getUsername()+"修改密码时原密码输入错误！");
+			result.put("errorMsg", "对不起，原密码输入错误！");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+
+	/**
+	 * 备份
+	 */
+	@RequestMapping("chuStudent")
+	public void chuStudent(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result = new JSONObject();
+		try {
+			String time = TimeUtil.formatTime(new Date(), "yyyyMMddHHmmss");
+		    String excelName = "手动备份"+time;
+		    List<StudentVo> list=studentService.findPage(new StudentVo());
+			String[] handers = {"序号","学生编号","姓名","年龄","所属年级","所选课程","入校时间"};
+			// 1导入硬盘
+			ExportExcelToDisk(request,handers,list, excelName);
+			// 2导出的位置放入attachment表
+			Attachment attachment = new Attachment();
+			attachment.setAttachmentname(excelName+".xls");
+			attachment.setAttachmentpath("logs/backup");
+			attachment.setAttachmenttime(new Date());
+			
+			result.put("success", true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put("", "对不起，备份失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	
+	
+	
+	// 导出到硬盘
+	@SuppressWarnings("resource")
+	private void ExportExcelToDisk(HttpServletRequest request,
+			String[] handers, List<StudentVo> list, String excleName) throws Exception {
+		
+		try {
+			HSSFWorkbook wb = new HSSFWorkbook();//创建工作簿
+			HSSFSheet sheet = wb.createSheet("操作记录备份");//第一个sheet
+			HSSFRow rowFirst = sheet.createRow(0);//第一个sheet第一行为标题
+			rowFirst.setHeight((short) 500);
+			for (int i = 0; i < handers.length; i++) {
+				sheet.setColumnWidth((short) i, (short) 4000);// 设置列宽
+			}
+			//写标题了
+			for (int i = 0; i < handers.length; i++) {
+			    //获取第一行的每一个单元格
+			    HSSFCell cell = rowFirst.createCell(i);
+			    //往单元格里面写入值
+			    cell.setCellValue(handers[i]);
+			}
+			for (int i = 0;i < list.size(); i++) {
+			    //获取list里面存在是数据集对象
+			    StudentVo log = list.get(i);
+			    //创建数据行
+			    HSSFRow row = sheet.createRow(i+1);
+			    //设置对应单元格的值
+			    row.setHeight((short)400);   // 设置每行的高度
+			    //"序号","操作人","IP地址","操作时间","操作模块","操作类型","详情"
+			    row.createCell(0).setCellValue(i+1);
+			    row.createCell(1).setCellValue(log.getCode());
+			    row.createCell(2).setCellValue(log.getName());
+			    row.createCell(3).setCellValue(log.getAge());
+			    row.createCell(4).setCellValue(log.getGrade());
+			    row.createCell(5).setCellValue(log.getCname());
+			    row.createCell(6).setCellValue(TimeUtil.formatTime(log.getEntrytime(), "yyyy-MM-dd"));
+			}
+			//写出文件（path为文件路径含文件名）
+				OutputStream os;
+				File file = new File("e:/tupian/"+excleName+".xls");
+				
+				if (!file.exists()){//若此目录不存在，则创建之  
+					file.createNewFile();  
+					logger.debug("创建文件夹路径为："+ file.getPath());  
+	            } 
+				os = new FileOutputStream(file);
+				wb.write(os);
+				os.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw e;
+			}
+	}
+
+
+}
---
+++ day09/zhangyuqi09/src/main/java/com/xiaoshu/service/StudentService.java
@@ -0,0 +1,117 @@
+package com.xiaoshu.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.github.pagehelper.util.StringUtil;
+import com.xiaoshu.dao.CourseMapper;
+import com.xiaoshu.dao.StudentMapper;
+
+import com.xiaoshu.entity.Course;
+import com.xiaoshu.entity.Student;
+import com.xiaoshu.entity.StudentExample;
+import com.xiaoshu.entity.StudentExample.Criteria;
+import com.xiaoshu.entity.StudentVo;
+
+
+
+@Service
+public class StudentService {
+	
+/*
+	@Autowired
+	UserMapper userMapper;
+
+	// 查询所有
+	public List<User> findUser(User t) throws Exception {
+		return userMapper.select(t);
+	};
+
+	// 数量
+	public int countUser(User t) throws Exception {
+		return userMapper.selectCount(t);
+	};
+
+	// 通过ID查询
+	public User findOneUser(Integer id) throws Exception {
+		return userMapper.selectByPrimaryKey(id);
+	};
+
+	
+	// 登录
+	public User loginUser(User user) throws Exception {
+		UserExample example = new UserExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andPasswordEqualTo(user.getPassword()).andUsernameEqualTo(user.getUsername());
+		List<User> userList = userMapper.selectByExample(example);
+		return userList.isEmpty()?null:userList.get(0);
+	};
+
+	// 通过角色判断是否存在
+	public User existUserWithRoleId(Integer roleId) throws Exception {
+		UserExample example = new UserExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andRoleidEqualTo(roleId);
+		List<User> userList = userMapper.selectByExample(example);
+		return userList.isEmpty()?null:userList.get(0);
+	}
+*/
+	@Autowired
+	private StudentMapper studentMapper;
+	
+	public PageInfo<StudentVo> findUserPage(StudentVo studentVo, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		List<StudentVo> userList = studentMapper.findPage(studentVo);
+		PageInfo<StudentVo> pageInfo = new PageInfo<StudentVo>(userList);
+		return pageInfo;
+	}
+@Autowired
+private CourseMapper courseMappers;
+	public List<Course> findById() {
+		// TODO Auto-generated method stub
+		return courseMappers.selectAll();
+	}
+	// 新增
+		public void addUser(Student t) throws Exception {
+			studentMapper.insert(t);
+		};
+
+		// 修改
+		public void updateUser(Student t) throws Exception {
+			studentMapper.updateByPrimaryKeySelective(t);
+		};
+
+		// 删除
+		public void deleteUser(Integer id) throws Exception {
+			studentMapper.deleteByPrimaryKey(id);
+		};
+
+		// 通过用户名判断是否存在，（新增时不能重名）
+		public Student existUserWithUserName(String userName) throws Exception {
+		StudentExample example=new StudentExample();
+			Criteria criteria = example.createCriteria();
+			criteria.andNameEqualTo(userName);
+			List<Student> userList = studentMapper.selectByExample(example);
+			return userList.isEmpty()?null:userList.get(0);
+		}
+		public List<StudentVo> findPage(StudentVo studentVo) {
+			// TODO Auto-generated method stub
+			return studentMapper.findPage(studentVo);
+		}
+		public List<StudentVo> EchartsStudent() {
+			// TODO Auto-generated method stub
+			return studentMapper.EchartsStudent();
+		}
+		public void reserveCourse(Course course) {
+			// TODO Auto-generated method stub
+			courseMappers.insert(course);
+		};
+
+
+
+}

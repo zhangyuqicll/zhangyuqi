5e606722107f05ac5521169de5435dae81659da8
zhangyuqi <2813945288@qq.com>
2020-08-12 18:40:58 +0800

    小实训day11作业

---
+++ day11/zhangyuqi11/src/main/java/com/xiaoshu/entity/ContentVo.java
@@ -0,0 +1,19 @@
+package com.xiaoshu.entity;
+
+public class ContentVo extends Content {
+		private String cname;
+		private Integer num;
+		public String getCname() {
+			return cname;
+		}
+		public void setCname(String cname) {
+			this.cname = cname;
+		}
+		public Integer getNum() {
+			return num;
+		}
+		public void setNum(Integer num) {
+			this.num = num;
+		}
+		
+}

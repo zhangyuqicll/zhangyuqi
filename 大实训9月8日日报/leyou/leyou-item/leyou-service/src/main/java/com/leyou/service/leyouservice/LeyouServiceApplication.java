541b4e448da9fa2de7bb75e172123a3165266401
zhangyuqi <2813945288@qq.com>
2020-09-08 19:19:10 +0800

    张愉其大实训9.8日报

---
+++ 大实训9月8日日报/leyou/leyou-item/leyou-service/src/main/java/com/leyou/service/leyouservice/LeyouServiceApplication.java
@@ -0,0 +1,13 @@
+package com.leyou.service.leyouservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeyouServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouServiceApplication.class, args);
+    }
+
+}
